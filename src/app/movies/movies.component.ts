8d66293c1c6ddbfc1d44fbd4d0bfb9aa98946d19
tknott95 <tknott95@hotmail.com>
2016-10-25 17:43:51 -0600

    Popular Movies Fetched

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -9,16 +9,9 @@ import { MovieService } from '../services/movie.service';
 })
 export class MoviesComponent {
 
-  constructor(private _movieService: MovieService) { }
-
-    getPopular() {
-      return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&sort_by=popularity.desc')
-        .map(res => res.json());
+    constructor(private _movieService: MovieService) { 
+        this._movieService.getPopular().subscribe(res => {
+          console.log(res.results);
+        });
     }
-
-    getTheaters() {
-      return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2016-26-15&primary_release_date.lte=2016-10-30')
-        .map(res => res.json());
-    }
-
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -10,6 +10,18 @@ export class MovieService{
     
     constructor(private _jsonp: Jsonp){
         this.apikey = 'ac7ca767d7e69df0b7981a908a39d499';
-        console.log(this.apikey);
+        console.log('MovieService Initialized..........');
     }
+
+    getPopular() {
+      return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&sort_by=popularity.desc&api_key='+this.apikey)
+        .map(res => res.json());
+    }
+
+    getTheaters() {
+      return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2016-26-15&primary_release_date.lte=2016-10-30&api_key='+this.apikey)
+        .map(res => res.json());
+    }
+
+
 }
