411af92221e405f4575bdb31416d9c74a985e414
tknott95 <tknott95@hotmail.com>
2016-10-25 17:38:57 -0600

    Setup API call functions

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MovieService } from '../services/movie.service';
 
 
@@ -7,11 +7,18 @@ import { MovieService } from '../services/movie.service';
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.css']
 })
-export class MoviesComponent implements OnInit {
+export class MoviesComponent {
 
   constructor(private _movieService: MovieService) { }
 
-  ngOnInit() {
-  }
+    getPopular() {
+      return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&sort_by=popularity.desc')
+        .map(res => res.json());
+    }
+
+    getTheaters() {
+      return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2016-26-15&primary_release_date.lte=2016-10-30')
+        .map(res => res.json());
+    }
 
 }
