4aeaece6af6ed162900ff735c7d622d93e0564dd
tknott95 <tknott95@hotmail.com>
2016-10-26 00:01:15 -0600

    Added Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,32 @@ import { HttpModule, JsonpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MovieComponent } from './movie/movie.component';
+import { RouterModule }   from '@angular/router';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
-    NavbarComponent
+    NavbarComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    JsonpModule
+    JsonpModule,
+    RouterModule.forRoot([
+      {
+    path: '',
+    component: MoviesComponent
+  },
+  {
+    path: 'movie/:id',
+    component: MovieComponent
+  }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
